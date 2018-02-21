5811e94f10fa2a47011fb1d70862f5a3cda3806c
Juliano <Juliano@Juliano-PC>
2018-02-21 15:08:09 -0300

    Alteração 2...

--- src/rede/neural/pkg1/RedeNeural1.java
+++ src/rede/neural/pkg1/RedeNeural1.java
@@ -15,7 +15,7 @@ public class RedeNeural1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Teste de PULL REQUEST");
+        System.out.println("Teste de PULL REQUEST modificado");
     }
     
 }
